2d0349a95bd143c44b953b5093505e3cae62d0b4
Jesse A. Johnson <jesseajohnson@pm.me>
2023-09-27 14:22:24 -0400

    add multiple versions of material and make them play nice

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,18 @@
 import { Button } from "@/module/one/OneMfe";
+import MuiButton from "@mui/material/Button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        Yarn v3 Berry Monorepo prototype
-        Using Button from @chariot/one that uses a different version of reactjs
+        Yarn v3 Berry Monorepo prototype with first Button from @chariot/one
+        that uses a different version of reactjs and a different version of mui
+        from the Root Theme Button
+        <br />
         <Button />
+        <br />
+        <MuiButton variant="contained">Root Theme Button</MuiButton>
       </div>
     </main>
-  )
+  );
 }
--- packages/one/src/Button.tsx
+++ packages/one/src/Button.tsx
@@ -1,16 +1,59 @@
 import React from "react";
 
+import MuiButton from "@material-ui/core/Button";
+
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  seed: 'one'
+});
+
+import {
+  createTheme,
+  makeStyles,
+  createStyles,
+  Theme as AugmentedTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
 type One = { name: string } | null;
 const INITIAL_ONE = { name: "foo" };
 const OneContext = React.createContext<One>(null);
 
 export function Button() {
   return (
-    <OneContext.Provider value={INITIAL_ONE}>
-      <div>
-        <div>My Button</div>
-        <button onClick={() => alert("hello world")} style={{ border: "2px solid purple" }}>CLICK ME</button>
-      </div>
-    </OneContext.Provider>
+    <StylesProvider generateClassName={generateClassName}>
+    <ThemeProvider theme={theme} key="OneMfe">
+      <OneContext.Provider value={INITIAL_ONE}>
+        <div>
+          <div>My Button</div>
+          <MuiButton
+            onClick={() => alert("hello world")}
+            variant="contained"
+            color="primary"
+          >
+            OneMfe Button
+          </MuiButton>
+        </div>
+      </OneContext.Provider>
+    </ThemeProvider>
+    </StylesProvider>
   );
 }
